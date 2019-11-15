e84ffbe62f646dbabc010811e72617a562f45183
Stijn van_der_Made <2120804@talnet.nl>
2019-11-15 13:48:04 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,7 @@
+<?php
+echo("voer een nummer in ");
+$nummer1 = readline();
+echo("voer nog een nummer in waarmee je het wilt vermenigvuldigen. ");
+$nummer2 = readline();
+print("het antwoord is " . $nummer1*$nummer2)
+?>
